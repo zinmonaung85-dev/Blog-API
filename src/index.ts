d985ee7977d744bfe4b032974401d8f6e18b07f0
Zin Mon Aung <zinmona017@gmail.com>
2026-06-12 10:43:54 +0630

    inserting data using prisma

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,88 @@
-const express = require("express");
-const dotenv = require("dotenv");
+import express from "express";
+import dotenv from "dotenv";
+import { prisma } from "./lib/prisma";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
-const app = express();
 
 if (!PORT) {
-    throw new Error("PORT is not provided");
+    console.error("PORT is not provided in your .env file.");
+    process.exit(1);
 }
 
-app.listen(Number(PORT), () => {
+const app = express();
+app.use(express.json());
+
+
+async function register() {
+    try {
+        const user = await prisma.user.create({
+            data: {
+                firstname: "Dili",
+                lastname: "Reba",
+                email: "dilireba@example.com",
+                password: "dilireba@26"
+            }
+        });
+
+        console.log(user);
+
+    } catch (error: unknown) {
+        if (error instanceof Error) {
+            console.error("Prisma Error:", error.message);
+        } else {
+            console.error("An unexpected error occurred:", error);
+        }
+    }
+}
+
+
+app.listen(Number(PORT), async () => {
     console.log(`✅ Express server running at http://localhost:${PORT}`);
+    await register();
 });
+
+// type User = {
+//     id: string;
+//     firstName: string;
+//     lastName: string;
+//     email: string;
+//     password: string;
+// };
+
+// const filePath = path.join(process.cwd(), "user.json");
+
+// const getUsers = (): User[] => {
+//     if (!fs.existsSync(filePath)) {
+//         fs.writeFileSync(filePath, JSON.stringify())
+//     }
+// }
+
+
+// // register user
+// app.post("/users", (req: any, res: any) => {
+//     const { firstName, lastName, email, password } = req.body;
+
+//     const newUser: User = {
+//         id: uuidv4(),
+//         firstName,
+//         lastName,
+//         email,
+//         password,
+//     };
+
+//     User.push(newUser);
+
+//     res.status(201).json({
+//         success: true,
+//         user: newUser,
+//     });
+// });
+
+
+//get user
+// app.get("/users", (req: any, res: any) => {
+//     res.status(200).json(users);
+// });
+
---
+++ src/lib/prisma.ts
@@ -0,0 +1,14 @@
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient } from "@prisma/client";
+import pg from "pg";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+
+const pool = new pg.Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+
+export const prisma = new PrismaClient({
+    adapter: adapter, // Restored the required adapter
+    log: ['query', 'info', 'warn', 'error'], // Keeps the SQL logging active
+});
