41f72d2a30649f33959e24259c9c1d229b8621eb
Zin Mon Aung <zinmona017@gmail.com>
2026-06-20 10:17:04 +0630

    like table setup

---
+++ prisma/migrations/20260620034457_add_like_table/migration.sql
@@ -0,0 +1,24 @@
+-- CreateTable
+CREATE TABLE "Like" (
+    "id" TEXT NOT NULL,
+    "userId" TEXT NOT NULL,
+    "blogId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Like_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE INDEX "Like_userId_idx" ON "Like"("userId");
+
+-- CreateIndex
+CREATE INDEX "Like_blogId_idx" ON "Like"("blogId");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Like_userId_blogId_key" ON "Like"("userId", "blogId");
+
+-- AddForeignKey
+ALTER TABLE "Like" ADD CONSTRAINT "Like_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Like" ADD CONSTRAINT "Like_blogId_fkey" FOREIGN KEY ("blogId") REFERENCES "Blog"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
